faba4dc601ec81aaf763fd1181701fb7197328a4
gbin2265 <gebiwork@hotmail.com>
2025-11-20 16:26:11 +0100

    recreate php with ai

--- application/controllers/ColumnexportController.php
+++ application/controllers/ColumnexportController.php
@@ -1,159 +1,157 @@
 <?php
 
-/** ColumnExport v1.0 **/
+/** ColumnExport v2.0 - Refactored **/
 
 namespace Icinga\Module\Columnexport\Controllers;
 
 use Icinga\Module\Icingadb\Web\Controller;
 use ipl\Web\Url;
 use ipl\Html\Html;
-use ipl\Web\Compat\CompatController;
-use Icinga\Application\Config;
 
 class ColumnexportController extends Controller
 {
-   /**
-     * Get URL base (Like .../icingaweb2)
-     *
-     * @var param_url_base
-   **/
-     public $param_url_base;
+    /**
+     * Supported export types with their corresponding icons
+     */
+    private const EXPORT_TYPES = [
+        'csv' => 'icon-file-excel',
+        'json' => 'icon-doc-text',
+        'pdf' => 'icon-file-pdf',
+        'sql' => 'icon-database'
+    ];
 
-   /**
-     * Get URL without the colums option
-     *
-     * @var param_url_without_columns
-   **/
-     public $param_url_without_columns;
+    /**
+     * Cached configuration entries
+     */
+    private array $configEntries = [];
 
-   /**
-     * Get URL without the colums option
-     *
-     * @var param_url_with_columns
-   **/
-     public $param_url_with_columns;
+    /**
+     * Main action for column export page
+     */
+    public function columnexportAction(): void
+    {
+        $pageType = $this->getPageType();
+        
+        $this->addTitleTab(sprintf(t('Export %s'), $pageType));
+        
+        $this->loadConfiguration($pageType);
+        
+        foreach (array_keys(self::EXPORT_TYPES) as $exportType) {
+            $this->renderExportSection($exportType);
+        }
+    }
 
-   /**
-     * Get URL option pagetype
-     *         Like : icingadb/hosts
-     *         Like : icingadb/services
-     *         Like : icingadb/hostgroups
-     *         Like : icingadb/servicegroups
-     *
-     * @var param_url_pagetype
-   **/
-     public $param_url_pagetype;
+    /**
+     * Get the page type from URL parameters
+     */
+    private function getPageType(): string
+    {
+        return strtolower($this->params->get('pagetype', ''));
+    }
 
-   /**
-     *
-     * Array with config.ini
-     *
-   **/
-     public $columnexport_config_ini;
+    /**
+     * Load and filter configuration entries for the given page type
+     */
+    private function loadConfiguration(string $pageType): void
+    {
+        $config = $this->Config();
+        $this->configEntries = [];
 
-   /**
-     * Action columnexport
-     *
-   **/
-     public function columnexportAction()
-     {  
-         /**
-         Get Parameters URL
-         **/
-
-         $this->param_url_base = Url::fromRequest()->getBasePath();
-         $this->param_url_without_columns = Url::fromRequest()->getParams()->without('columns')->without('pagetype')->toString();
-         $this->param_url_with_columns = Url::fromRequest()->getParams()->without('pagetype')->toString();
-         $this->param_url_pagetype = strtolower($this->params->get('pagetype'));
-
-         /**
-         Create Page
-         **/
-         $this->addTitleTab(t('Export ' . $this->param_url_pagetype));
-
-         $this->readColumnExportIniFile ( $this->param_url_pagetype );
-
-         $this->createColumnExportHtml( 'csv' );
-         $this->createColumnExportHtml( 'json' );
-         $this->createColumnExportHtml( 'pdf' );
-         $this->createColumnExportHtml( 'sql' );
-     }
-
-   /**
-     * Read config.ini file
-     *
-   **/
-     protected function readColumnExportIniFile ( $filter_urltype ) 
-     {
-         /** columnexport/config.ini **/
-         $config = $this->Config();
-
-         /** Clean Array columnexport_config_ini **/
-         $this->columnexport_config_ini = [];
-    
-         /** Read every key form config.ini where urltype is ... **/ 
-         foreach ($config as $key => $part) {
-            $ini_urltype = strtolower($part->get('urltype'));
-            if ( $filter_urltype === $ini_urltype ) { 
-               $config_ini = [
-                               'config_ini_title' => $part->get('title'),
-                               'config_ini_urltype' => $ini_urltype,
-                               'config_ini_exporttype' => strtolower($part->get('exporttype')),
-                               'config_ini_columns' => $part->get('columns')
-                              ];
-               array_push($this->columnexport_config_ini , $config_ini );
+        foreach ($config as $section) {
+            $urlType = strtolower($section->get('urltype', ''));
+            
+            if ($urlType === $pageType) {
+                $this->configEntries[] = [
+                    'title' => $section->get('title'),
+                    'urltype' => $urlType,
+                    'exporttype' => strtolower($section->get('exporttype', '')),
+                    'columns' => $section->get('columns')
+                ];
             }
-         }
-     }
+        }
+    }
 
+    /**
+     * Render export section for a specific export type
+     */
+    private function renderExportSection(string $exportType): void
+    {
+        $div = Html::tag('div', ['class' => 'content']);
+        $div->add(Html::tag('h2', Html::tag('u', strtoupper($exportType))));
 
-   /**
-     * Create Html Page
-     *
-   **/
-     protected function createColumnExportHtml ( $TypeColumnExport )
-     {
+        $entries = $this->getEntriesByExportType($exportType);
+        
+        foreach ($entries as $entry) {
+            $div->add($this->createExportLink($entry, $exportType));
+        }
 
-           /** Header for Type Export **/
-           $div = Html::tag('div',['class'=>"content"]);
-           $headertext = Html::tag('h2', Html::tag('u',strtoupper($TypeColumnExport)));
-           $div->add($headertext);
-           $this->addContent($div);
+        $this->addContent($div);
+    }
 
-           /** Create every html line for Type Export **/
-           foreach ($this->columnexport_config_ini as $index => $item) {
-              $ini_urltype = $item['config_ini_urltype'];
-              if ( $this->param_url_pagetype === $ini_urltype ) { 
-                   $ini_exporttype = $item['config_ini_exporttype'];
-                   if ( $TypeColumnExport === $ini_exporttype ) { 
-                        switch ($TypeColumnExport) {
-                          case "json":
-                               $exportclass = 'icon-doc-text'; 
-                               break;
-                          case "pdf":
-                                $exportclass = 'icon-file-pdf'; 
-                                break;
-                          case "sql":
-                                $exportclass = 'icon-database'; 
-                                break;
-                          default:
-                                $exportclass = 'icon-file-excel';
-                        }    
-                        $ini_title = $item['config_ini_title'];
-                        $ini_columns = $item['config_ini_columns'];
-                        if ( ! empty($ini_columns) ) {
-                             $columnexport_content = $ini_title . ' (columns=' . $ini_columns . ')';
-                             $columnexport_url = $this->param_url_base . '/' . $this->param_url_pagetype . '?' . $this->param_url_without_columns . '&columns=' . $ini_columns . '&format=' . $TypeColumnExport;
-                        }
-                        else {
-                             $columnexport_content = $ini_title;
-                             $columnexport_url = $this->param_url_base . '/' . $this->param_url_pagetype . '?' . $this->param_url_with_columns . '&format=' . $TypeColumnExport;
-                        }
-                        $columnexport_html = Html::tag('li', null , Html::tag('a', ['href' => $columnexport_url , 'target' => '_blank' , 'class' => $exportclass], $columnexport_content));
-                        $div->add($columnexport_html);
-                   }
-              }
-           }
-     }
+    /**
+     * Filter configuration entries by export type
+     */
+    private function getEntriesByExportType(string $exportType): array
+    {
+        return array_filter(
+            $this->configEntries,
+            fn($entry) => $entry['exporttype'] === $exportType
+        );
+    }
 
+    /**
+     * Create an export link HTML element
+     */
+    private function createExportLink(array $entry, string $exportType): object
+    {
+        $url = $this->buildExportUrl($entry, $exportType);
+        $content = $this->buildLinkContent($entry);
+        $icon = self::EXPORT_TYPES[$exportType] ?? 'icon-file-excel';
+
+        return Html::tag('li', null, 
+            Html::tag('a', [
+                'href' => $url,
+                'target' => '_blank',
+                'class' => $icon
+            ], $content)
+        );
+    }
+
+    /**
+     * Build the export URL with appropriate parameters
+     */
+    private function buildExportUrl(array $entry, string $exportType): string
+    {
+        $urlBase = Url::fromRequest()->getBasePath();
+        $pageType = $this->getPageType();
+        $params = Url::fromRequest()->getParams();
+        
+        // Remove unnecessary parameters
+        $cleanParams = $params->without('columns', 'pagetype', 'format');
+        
+        // Add columns if specified in config
+        if (!empty($entry['columns'])) {
+            $cleanParams->set('columns', $entry['columns']);
+        }
+        
+        // Add format parameter
+        $cleanParams->set('format', $exportType);
+        
+        return sprintf('%s/%s?%s', $urlBase, $pageType, $cleanParams->toString());
+    }
+
+    /**
+     * Build the link content text
+     */
+    private function buildLinkContent(array $entry): string
+    {
+        $title = $entry['title'];
+        $columns = $entry['columns'];
+
+        if (!empty($columns)) {
+            return sprintf('%s (columns=%s)', $title, $columns);
+        }
+
+        return $title;
+    }
 }
